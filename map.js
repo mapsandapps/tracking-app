728e8e158fbaf85f88db919a280a6c0270c8dc71
Shawn Taylor <staylor@cybraics.com>
2017-10-29 14:22:29 -0400

    Click through each track

--- map.js
+++ map.js
@@ -6,13 +6,18 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapsandapps/cj9bj48ts4cza2rn2avra7
   maxZoom: 18
 }).addTo(map);
 
-// TODO: have tracks load one at a time; pause after each to take screenshot
-// TODO: and let you insert some text about that route segment
-// TODO: have button that takes the screenshot for you
+// TODO: allow user to insert some text about each route segment
+// TODO: add legend
+// TODO: display the date
 
-var layersLoaded = 0;
 var totalLayers;
 var mapBounds = L.latLngBounds([]);
+var mappedFiles = 0;
+var mapLayers = [];
+var lastLayer;
+var settings = {
+  fitMapTo: 'fit-to-all'
+};
 
 function handleFileSelect(e) {
   const files = e.target.files;
@@ -21,9 +26,8 @@ function handleFileSelect(e) {
     const reader = new FileReader();
     reader.readAsText(file);
     reader.onload = () => {
-      layersLoaded += 1;
       var fileContents = reader.result;
-      addFileToMap(fileContents);
+      addFileContentsToList(fileContents);
     }
   });
 }
@@ -44,20 +48,56 @@ function getColorByMode(mode) {
   }
 }
 
-function addFileToMap(fileContents) {
-  var currentLayer = omnivore.gpx.parse(fileContents);
-  currentLayer.setStyle({
-    opacity: 0.5,
-    weight: 8,
-    color: getColorByMode(currentLayer.toGeoJSON().features[0].properties.name.split(/( \d)/)[0])
+function lightenLastLayer(layer) {
+  layer.setStyle({
+    opacity: 0.3
   });
-  currentLayer.addTo(map);
-  mapBounds.extend(currentLayer.getBounds());
+}
 
-  // if this is the last one, do other stuff
-  if (layersLoaded === totalLayers) {
-    console.log('done!');
+function addLayerToMap(layer) {
+  if (lastLayer) {
+    lightenLastLayer(lastLayer);
+  }
+  if (layer) { // for the last click, there's no new layer; just lowering opacity of last layer and zooming out
+    layer.setStyle({
+      opacity: 1,
+      weight: 8,
+      color: getColorByMode(layer.mode)
+    });
+    lastLayer = layer;
+    layer.addTo(map);
+    mapBounds.extend(layer.getBounds());
+    if (settings.fitMapTo === 'fit-to-each') {
+      map.fitBounds(layer.getBounds());
+    } else {
+      map.fitBounds(mapBounds);
+    }
+  } else {
     map.fitBounds(mapBounds);
+  }
+}
+
+function createMapLayer(path) {
+  var currentLayer = omnivore.gpx.parse(path);
+  mapLayers.push(currentLayer);
+}
+
+function addFileContentsToList(fileContents) {
+  createMapLayer(fileContents);
+
+  // when the last file is processed, do other stuff
+  if (mapLayers.length === totalLayers) {
+
+    _.forEach(mapLayers, layer => {
+      layer.time = layer.toGeoJSON().features[0].properties.time;
+      layer.mode = layer.toGeoJSON().features[0].properties.name.split(/( \d)/)[0]
+    });
+
+    mapLayers = _.sortBy(mapLayers, ['time']);
+
+    document.getElementById('track-interactions').classList.remove('hidden');
+    document.getElementById('total-tracks').innerHTML = totalLayers;
+    advanceTrack(); // show first track
   }
 }
 
@@ -77,13 +117,7 @@ function createImage() {
       var svgPaths = document.getElementsByTagName('g');
       _.forEach(svgPaths, path => {
         context.drawSvg(path.innerHTML, 0, 0, 0, 0);
-      })
-
-      // canvg(tempCanvas, svgPaths.innerHTML, {
-      //   log: true,
-      //   ignoreAnimation: true
-      // });
-      // context.drawSvg(svgPaths.innerHTML, 0, 0, 0, 0);
+      });
 
       var link = document.createElement("a");
       link.href = tempCanvas.toDataURL('image/png');
@@ -92,3 +126,18 @@ function createImage() {
     }
   });
 }
+
+function advanceTrack() {
+  addLayerToMap(mapLayers[mappedFiles]);
+  mappedFiles += 1;
+  document.getElementById('completed-tracks').innerHTML = mappedFiles > totalLayers ? 'All' : mappedFiles;
+
+  if (mappedFiles === totalLayers + 1) {
+    document.getElementById('advance-button').disabled = true;
+  }
+}
+
+var form = document.getElementById('fit-map');
+form.addEventListener('change', event => {
+  settings.fitMapTo = event.target.id;
+});
